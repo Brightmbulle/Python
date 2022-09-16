5e56f7bf45891c471777649b5d3fa16de866a4d7
Atagah Bright <brightmbulle@gmail.com>
2022-09-15 20:40:49 -0800

    Create program1.py
    This python program lets you input 3 random numbers, and the if statement is conditioned to print the corresponding output depending on the size of the number

---
+++ If Statement/program1.py
@@ -0,0 +1,8 @@
+a = input ("Please enter the first number")
+b = input ("Please enter the second number")
+if a > b:
+    print ("The first number is bigger than the second")
+elif a < b:
+    print ("The first number is smaller than the second")
+else:
+    print ("The two numbers are equal")
